c3e0d3d5352e89ea69d8a995dd1e8152e46103d5
Marlon Santos <marlutcu@gmail.com>
2019-09-28 16:01:49 -0300

    add base structur for formCategory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
+import {ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -16,6 +17,7 @@ import {InMemoryDataBase} from './in-memory-database';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    ReactiveFormsModule,
     HttpClientInMemoryWebApiModule.forRoot(InMemoryDataBase)
   ],
   providers: [],
--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -1,4 +1,9 @@
+import { CategoryService } from './../shared/category.service';
+import { Category } from './../shared/category.model';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import {FormGroup, FormBuilder, Validators} from '@angular/forms';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-category-form',
@@ -7,9 +12,74 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryFormComponent implements OnInit {
 
-  constructor() { }
+  currentAction: string;
+  categoryForm: FormGroup;
+  pageTitle: string;
+  serverErrorMessages: string[] = null;
+  submitingForm: boolean =  false;
+  category: Category = new Category();
+
+  constructor(
+    private categoryService: CategoryService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.setCurrentAction();
+    this.buildCatgoryForm();
+    this.loadCategory();
   }
+
+  ngAfterContentChecked(){
+    this.setPageTitle();
+  }
+  
+ 
+  //private methods
+  private setCurrentAction() {
+    if(this.route.snapshot.url[0].path == 'new')
+      this.currentAction = 'new';
+    else
+      this.currentAction = 'edit';
+  }
+
+  private buildCatgoryForm() {
+    this.categoryForm = this.formBuilder.group({
+      id: [null],
+      name: [null, [Validators.required,Validators.minLength(2)]],
+      description: [null]
+    })
+  }
+
+  private  loadCategory() {
+    if(this.currentAction == 'edit'){
+      this.route.paramMap.pipe(
+        switchMap(params => this.categoryService.getById(+params.get('id')))
+      )
+      .subscribe(
+        (category) => {
+          this.category = category;
+          this.categoryForm.patchValue(category)
+        },
+        (error) => {
+          alert('Ocorreu um erro no servidor, tente novamente mais tarde');
+        }
+      )
+    }
+  }
+
+  private setPageTitle() {
+    if(this.currentAction == 'new'){
+      this.pageTitle = "Cadastrar nova categoria";
+    }
+    else{
+      const categoryName = this.category.name || "";
+      this.pageTitle = `Editando Categoria: ${categoryName}`;
+    }
+  }
+  
+  
 
 }
